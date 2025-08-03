25f39eed70909d5f120dc7de93e56ef18b773561
Alexander <aaalexander2123@gmail.com>
2025-08-02 20:36:01 -0500

    se añaden los models al admin

--- appGestor/admin.py
+++ appGestor/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from votos.models import Roles, Programas, Ficha, Estado, TipoDocumento, Usuario, Password, Candidato, Propuesta, Voto, FechaVotacion
 
-# Register your models here.
+admin.site.register(Roles)
+admin.site.register(Programas)
+admin.site.register(Ficha)
+admin.site.register(Estado)
+admin.site.register(TipoDocumento)
+admin.site.register(Usuario)
+admin.site.register(Password)
+admin.site.register(Candidato)
